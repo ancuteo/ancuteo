a2fafe3e4b87df03179120b4ce783cd8b01ec7fc
ancuteo <anantivi2003@gmail.com>
2026-01-31 09:58:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ async def get_token(acc, session):
         if not uid:
             return None
 
-        url = f"https://api-jwt-theta.vercel.app/token?uid={uid}&password={password}"
+        url = f"https://api-jwt-eight.vercel.app/token?uid={uid}&password={password}"
         async with session.get(url) as response:
             text = await response.text()
 
